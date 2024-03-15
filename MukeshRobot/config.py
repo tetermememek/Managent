00ffa20ef5f98ac72989df22401d2e038a2e2c2d
petinggint <155607304+petinggint@users.noreply.github.com>
2024-03-15 23:51:41 +0700

    W config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,9 +7,9 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 7054610436 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "berlinmusic_support"  # Your own group for support, do not add the @
     START_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
@@ -25,12 +25,12 @@ class Config(object):
     #generate api from telegram using /token command bot username :>> @adventure_robot
 
     # Optional fields
-    BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
-    DEMONS = []  # User id of support users
-    TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    BL_CHATS = [7054610436]  # List of groups that you want blacklisted.
+    DRAGONS = [7054610436]  # User id of sudo users
+    DEV_USERS = [7054610436]  # User id of dev users
+    DEMONS = [7054610436]  # User id of support users
+    TIGERS = [7054610436]  # User id of tiger users
+    WOLVES = [7054610436]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
